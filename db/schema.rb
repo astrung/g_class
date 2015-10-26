22f2b23402f8d6861766326ebf598601c0a456a6
astrung <tuananh93nguyen@gmail.com>
2015-10-26 06:53:04 +0000

    add comlumn for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012060107) do
+ActiveRecord::Schema.define(version: 20151026060601) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 20151012060107) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "gender"
+    t.integer  "age"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
